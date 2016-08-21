34903662e22f31291ea03571283e0c872005e870
Michael Hii <mslhii@yahoo.com>
2016-08-21 01:20:09 -0700

    Testing permissions

--- app/src/main/java/com/kritikalerror/instagramuploader/MainActivity.java
+++ app/src/main/java/com/kritikalerror/instagramuploader/MainActivity.java
@@ -53,7 +53,8 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         mContext = this;
-        initialize();
+        //initialize();
+        initializeWrapper();
         mPath = "";
     }
 
